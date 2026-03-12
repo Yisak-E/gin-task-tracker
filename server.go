0bc940fc273a718b168ce20c0db4bf413ab46e90
Yisak-E <yisakmetaferiya1@gmail.com>
2026-03-12 06:07:47 +0400

    initiate the gin task tracker

---
+++ server.go
@@ -0,0 +1,16 @@
+package gin_task_tracker
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	server.Run(":8080")
+
+}
